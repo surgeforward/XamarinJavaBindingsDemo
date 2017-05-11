ecc0016faa04e5a9e7513bf115be624d283bf6d0
Andrew Hoefling <ahoefling@surgeforward.com>
2017-05-11 17:58:30 +0000

    wired up commands and added listener objects to audio service

--- XamarinJavaBindingsDemo/XamarinJavaBindingsDemo.Android/Services/AudioService.cs
+++ XamarinJavaBindingsDemo/XamarinJavaBindingsDemo.Android/Services/AudioService.cs
@@ -1,12 +1,13 @@
-
 using Android.App;
 using Com.Github.Lassana.Recorder;
+using static XamarinJavaBindingsDemo.Droid.Services.AudioService;
 
 namespace XamarinJavaBindingsDemo.Droid.Services
 {
-    public class AudioService : IAudioService
+    public class AudioService : IAudioService, IAudioServiceMetadata
     {
         AudioRecorder _recorder;
+
         public AudioService()
         {
             _recorder = AudioRecorderBuilder.With(Application.Context)
@@ -18,12 +19,57 @@ namespace XamarinJavaBindingsDemo.Droid.Services
 
         public void Start()
         {
-            _recorder.Start(null);
+            _recorder.Start(new OnStartListener(this));
         }
 
         public void Pause()
         {
-            _recorder.Pause(null);
+            _recorder.Pause(new OnPauseListener(this));
+        }
+
+        internal interface IAudioServiceMetadata
+        {
+            //Timer RecordingTimer { get; }
+            //bool IsRecording { get; set; }
+        }
+        private class OnPauseListener : Listener, AudioRecorder.IOnPauseListener
+        {
+            public OnPauseListener(IAudioServiceMetadata metadata)
+                : base(metadata) { }
+
+            public void OnException(Java.Lang.Exception p0)
+            {
+            }
+
+            public void OnPaused(string activeFileName)
+            {
+                //Metadata.RecordingTimer.Stop();
+                //Metadata.IsRecording = false;
+            }
+        }
+        private class OnStartListener : Listener, AudioRecorder.IOnStartListener
+        {
+            public OnStartListener(IAudioServiceMetadata metadata)
+                : base(metadata) { }
+
+            public void OnException(Java.Lang.Exception p0)
+            {
+            }
+
+            public void OnStarted()
+            {
+                //Metadata.RecordingTimer.Start();
+                //Metadata.IsRecording = true;
+            }
+        }
+        private abstract class Listener : Java.Lang.Object
+        {
+            protected IAudioServiceMetadata Metadata { get; }
+
+            protected Listener(IAudioServiceMetadata metadata)
+            {
+                Metadata = metadata;
+            }
         }
     }
 }
