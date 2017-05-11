5f29bc60964ad47607238d498c112f27654096fb
Andrew Hoefling <ahoefling@surgeforward.com>
2017-05-11 17:21:31 +0000

    adding methods start and pause to the audio service interface

--- XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/IAudioService.cs
+++ XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/IAudioService.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XamarinJavaBindingsDemo
+﻿namespace XamarinJavaBindingsDemo
 {
     public interface IAudioService
     {
+        void Start();
+        void Pause();
     }
 }
