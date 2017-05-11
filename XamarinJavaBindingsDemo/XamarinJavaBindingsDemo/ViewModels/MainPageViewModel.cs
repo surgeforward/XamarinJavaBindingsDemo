65e482ea87f3831b602aeaf7cafa29e26f27d53c
Andrew Hoefling <ahoefling@surgeforward.com>
2017-05-11 17:43:57 +0000

    adding bindings

--- XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/ViewModels/MainPageViewModel.cs
+++ XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/ViewModels/MainPageViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace XamarinJavaBindingsDemo.ViewModels
 {
     public class MainPageViewModel
     {
         public string Title => "Xamarin Java Bindings Demo";
+        public IAudioService _audioService;
+
+        public ICommand Start { get; }
+        public ICommand Stop { get; }
+
+        public MainPageViewModel(IAudioService audioService)
+        {
+            _audioService = audioService;
+
+            Start = new DelegateCommand(OnStart);
+            Stop = new DelegateCommand(OnStop);
+        }
+
+        public void OnStart()
+        {
+            _audioService.Start();
+        }
+
+        public void OnStop()
+        {
+            _audioService.Pause();
+        }
     }
 }
