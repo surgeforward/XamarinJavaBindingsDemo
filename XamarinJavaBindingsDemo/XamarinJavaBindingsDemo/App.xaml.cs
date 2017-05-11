b183377f4144d84c4aa865681303443e111d76b4
Andrew Hoefling <ahoefling@surgeforward.com>
2017-05-11 17:18:21 +0000

    added prism and creaed app code for unity wire up

--- XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/App.xaml.cs
+++ XamarinJavaBindingsDemo/XamarinJavaBindingsDemo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,28 +8,20 @@ using Xamarin.Forms;
 
 namespace XamarinJavaBindingsDemo
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
         {
             InitializeComponent();
-
-            MainPage = new XamarinJavaBindingsDemo.MainPage();
+            NavigationService.NavigateAsync("Navigation/Main");
         }
 
-        protected override void OnStart()
+        protected override void RegisterTypes()
         {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
         }
     }
 }
